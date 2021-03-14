fe56f98e40b4cfec5a6d163e2e6e6cbd43506f55
David Löfstrand <davidlofstrand@gmail.com>
2021-03-14 14:50:22 +0100

    Added test for failing case

--- src/cpp/UnitTests/ddmatch/main.cpp
+++ src/cpp/UnitTests/ddmatch/main.cpp
@@ -1,7 +1,14 @@
 #include <gtest/gtest.h>
 
-TEST(dummy, test) {
-  EXPECT_EQ(1,1);
+#include "ddmatch/Diffeo_functions.h"
+
+
+TEST(Diffeo_functionsTest, boundary_conditions1) {
+  const auto [i0, i1, sh0, sh1, frac] = boundary_conditions1(-1e-16, 64);
+  EXPECT_EQ(i0, 63);
+  EXPECT_EQ(i1, 0);
+  EXPECT_NEAR(sh0, -64.0, 0.01);
+  EXPECT_NEAR(sh1, 0.0, 0.01);
 }
 
 int main(int argc, char** argv) {
