be8dfa5f078899287c79558a8b971de71dc37921
David Löfstrand <davidlofstrand@gmail.com>
2021-03-15 20:40:23 +0100

    Removed unused functions from MyArrays. Added more unit tests for core/MyArrays.h

--- src/cpp/UnitTests/core/test_TGrid.cpp
+++ src/cpp/UnitTests/core/test_TGrid.cpp
@@ -10,7 +10,6 @@ TEST(TGrid_Test, DefaultConstructor) {
     EXPECT_EQ(def.size(), 0);
     EXPECT_EQ(def.size_bytes(), 0);
     EXPECT_EQ(def.data(), nullptr);
-    EXPECT_DEATH(def[0][0], "");
   }
   {
     TGrid<float> def;
@@ -19,7 +18,6 @@ TEST(TGrid_Test, DefaultConstructor) {
     EXPECT_EQ(def.size(), 0);
     EXPECT_EQ(def.size_bytes(), 0);
     EXPECT_EQ(def.data(), nullptr);
-    EXPECT_DEATH(def[0][0], "");
   }
   {
     TGrid<double> def;
@@ -28,7 +26,6 @@ TEST(TGrid_Test, DefaultConstructor) {
     EXPECT_EQ(def.size(), 0);
     EXPECT_EQ(def.size_bytes(), 0);
     EXPECT_EQ(def.data(), nullptr);
-    EXPECT_DEATH(def[0][0], "");
   }
 }
 
@@ -77,3 +74,115 @@ TEST(TGrid_Test, Constructor) {
   }
 }
 
+TEST(TGrid_Test, OutOfBounds) {
+  TGrid<int> grid(2,2, 0);
+  EXPECT_DEATH(grid[-1][0], "");
+  EXPECT_DEATH(grid[0][-1], "");
+  EXPECT_DEATH(grid[-1][-1], "");
+  EXPECT_DEATH(grid[2][0], "");
+  EXPECT_DEATH(grid[0][2], "");
+  EXPECT_DEATH(grid[2][2], "");
+}
+
+
+TEST(TGrid_Test, values_like) {
+  {
+    TGrid<int> grid_orig(3,4, -1);
+    TGrid<int> grid_test = values_like(grid_orig, 2);
+    EXPECT_EQ(grid_test.rows(), 3);
+    EXPECT_EQ(grid_test.cols(), 4);
+    EXPECT_EQ(grid_test.size(), 12);
+    EXPECT_EQ(grid_test.size_bytes(), 12 * sizeof(int));
+    int* p = grid_test.data();
+    ASSERT_NE(p, nullptr);
+    for(int i = 0; i < grid_test.size(); ++i)
+      EXPECT_EQ(p[i], 2) << " element " << i << " has unexpected value";
+  }
+}
+
+TEST(TGrid_Test, zeros_like) {
+  {
+    TGrid<int> grid_orig(4,3, -1);
+    TGrid<int> grid_test = zeros_like(grid_orig);
+    EXPECT_EQ(grid_test.rows(), 4);
+    EXPECT_EQ(grid_test.cols(), 3);
+    EXPECT_EQ(grid_test.size(), 12);
+    EXPECT_EQ(grid_test.size_bytes(), 12 * sizeof(int));
+    int* p = grid_test.data();
+    ASSERT_NE(p, nullptr);
+    for(int i = 0; i < grid_test.size(); ++i)
+      EXPECT_EQ(p[i], 0) << " element " << i << " has unexpected value";
+  }
+}
+
+TEST(TGrid_Test, ones_like) {
+  {
+    TGrid<int> grid_orig(2,2, -1);
+    TGrid<int> grid_test = ones_like(grid_orig);
+    EXPECT_EQ(grid_test.rows(), 2);
+    EXPECT_EQ(grid_test.cols(), 2);
+    EXPECT_EQ(grid_test.size(), 4);
+    EXPECT_EQ(grid_test.size_bytes(), 4 * sizeof(int));
+    int* p = grid_test.data();
+    ASSERT_NE(p, nullptr);
+    for(int i = 0; i < grid_test.size(); ++i)
+      EXPECT_EQ(p[i], 1) << " element " << i << " has unexpected value";
+  }
+}
+
+TEST(TGrid_Test, copyto_TGrid_TGrid) {
+  {
+    TGrid<int> grid_a(2,2, -1);
+    TGrid<int> grid_b(2,3, -2);
+    EXPECT_FALSE(copyto(grid_a, grid_b));
+    EXPECT_EQ(grid_a.rows(), 2);
+    EXPECT_EQ(grid_a.cols(), 2);
+    int* p = grid_a.data();
+    ASSERT_NE(p, nullptr);
+    for(int i = 0; i < grid_a.size(); ++i)
+      EXPECT_EQ(p[i], -1) << " element " << i << " has unexpected value";
+  }
+
+  {
+    TGrid<int> grid_a(2,2, -1);
+    TGrid<int> grid_b(2,2, 4);
+    EXPECT_TRUE(copyto(grid_a, grid_b));
+    EXPECT_EQ(grid_a.rows(), 2);
+    EXPECT_EQ(grid_a.cols(), 2);
+    int* p = grid_a.data();
+    ASSERT_NE(p, nullptr);
+    for(int i = 0; i < grid_a.size(); ++i)
+      EXPECT_EQ(p[i], 4) << " element " << i << " has unexpected value";
+  }
+}
+
+TEST(TGrid_Test, copyto_TVecRef_stdvec) {
+  {
+    TGrid<int> grid_a(2,2, -1);
+    std::vector<int> b{1,2,3};
+    EXPECT_FALSE(copyto(grid_a[0], b));
+    EXPECT_FALSE(copyto(grid_a[1], b));
+    EXPECT_EQ(grid_a.rows(), 2);
+    EXPECT_EQ(grid_a.cols(), 2);
+    int* p = grid_a.data();
+    ASSERT_NE(p, nullptr);
+    for(int i = 0; i < grid_a.size(); ++i)
+      EXPECT_EQ(p[i], -1) << " element " << i << " has unexpected value";
+  }
+
+  {
+    TGrid<int> grid_a(2,2, -1);
+    std::vector<int> b{1,2};
+    std::vector<int> c{3,4};
+    EXPECT_TRUE(copyto(grid_a[0], b));
+    EXPECT_TRUE(copyto(grid_a[1], c));
+    EXPECT_EQ(grid_a.rows(), 2);
+    EXPECT_EQ(grid_a.cols(), 2);
+    EXPECT_EQ(grid_a[0][0], 1);
+    EXPECT_EQ(grid_a[0][1], 2);
+    EXPECT_EQ(grid_a[1][0], 3);
+    EXPECT_EQ(grid_a[1][1], 4);
+  }
+}
+
+// elem_func
